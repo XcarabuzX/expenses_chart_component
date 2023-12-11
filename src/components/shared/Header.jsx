f1e7ca345644a11d96ade3b07f8da4c85d4266fe
XcarabuzX <xcarabuzx@gmail.com>
2023-12-11 17:59:40 -0300

    🚧 Header

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,8 +1,10 @@
+import logoSVG from '../../assets/img/logo.svg?react';
 function Header() {
     return ( 
         <header>
             <small>My balance</small>
             <p>921.48</p>
+            <logoSVG></logoSVG>
         </header>
      );
 }
